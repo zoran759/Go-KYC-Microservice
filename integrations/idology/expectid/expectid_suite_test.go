bd80747451737853ca08dfb71db86885f77a3f01
Zoran Rasic <zokiupwork@gmail.com>
2018-08-26 17:26:52 +0300

    IDology:
    * use "Street" field of the current address for the street address.

--- integrations/idology/expectid/expectid_suite_test.go
+++ integrations/idology/expectid/expectid_suite_test.go
@@ -1,11 +1,15 @@
 package expectid
 
 import (
+	"flag"
 	"testing"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
+
+// It is here for compatibility with recursive running of tests for IDology.
+var runLive = flag.Bool("runlive", false, "Don't use this flag in this context.")
 
 func TestExpectID(t *testing.T) {
 	RegisterFailHandler(Fail)
