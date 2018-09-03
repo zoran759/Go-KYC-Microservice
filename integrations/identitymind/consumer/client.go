7190650ada4fecd4cc9dd349e70e71008c5d5589
Zoran Rasic <zokiupwork@gmail.com>
2018-09-03 22:58:54 +0300

    IDM: current work.

--- integrations/identitymind/consumer/client.go
+++ integrations/identitymind/consumer/client.go
@@ -1,14 +1,21 @@
 package consumer
 
 import (
+	"encoding/base64"
+	"encoding/json"
 	"errors"
+	"fmt"
 
 	"gitlab.com/lambospeed/kyc/common"
+	"gitlab.com/lambospeed/kyc/http"
 )
 
 const (
-	consumerEndpoint    = "/account/consumer"
-	filesUploadEndpoint = "/account/consumer/%s/files"
+	contentType = "application/json"
+
+	consumerEndpoint       = "/account/consumer"
+	filesUploadEndpoint    = "/account/consumer/%s/files"
+	stateRetrievalEndpoint = "/account/consumer/v2/%s"
 )
 
 // Client defines the client for IdentityMind API.
@@ -27,12 +34,72 @@ func NewClient(config Config) *Client {
 
 // CheckCustomer implements customer verification using IdentityMind API.
 func (c *Client) CheckCustomer(customer *common.UserData) (result common.KYCResult, details *common.DetailedKYCResult, err error) {
-	// TODO: implement this.
+	result = common.Error
+
 	if customer == nil {
-		result = common.Error
 		err = errors.New("no customer supplied")
 		return
 	}
+
+	req := &KYCRequestData{}
+	if err = req.populateFields(customer); err != nil {
+		err = fmt.Errorf("invalid customer data: %s", err)
+		return
+	}
+
+	body, err := req.createRequestBody()
+	if err != nil {
+		err = fmt.Errorf("during creating request body: %s", err)
+		return
+	}
+
+	response, err := c.sendRequest(body)
+	if err != nil {
+		err = fmt.Errorf("during sending request: %s", err)
+		return
+	}
+
+	// FIXME: I can't understand from the available docs how documents verification is doing and how to implement it.
+
+	if response.State == UnderReview {
+		response, err = c.pollApplicationState(response.KYCTxID)
+		if err != nil {
+			err = fmt.Errorf("during retrieving current KYC state: %s", err)
+			return
+		}
+	}
+
+	result, details, err = response.toResult()
+
+	return
+}
+
+// sendRequest sends a vefirication request into the API.
+// It returns a response from the API or the error if occured.
+func (c *Client) sendRequest(body []byte) (response *ApplicationResponseData, err error) {
+	auth := base64.StdEncoding.EncodeToString([]byte(c.config.Username + ":" + c.config.Password))
+
+	headers := http.Headers{
+		"Content-Type":  contentType,
+		"Authorization": "Basic " + auth,
+	}
+
+	_, resp, err := http.Post(c.config.Host+consumerEndpoint, headers, body)
+	if err != nil {
+		return
+	}
+
+	response = &ApplicationResponseData{}
+	err = json.Unmarshal(resp, response)
+
+	return
+}
+
+// pollApplicationState polls IDM API for the current state of a consumer KYC so long
+// as the returned state is "Under Review".
+// If the application is not found then an error message is provided in the response.
+func (c *Client) pollApplicationState(mtid string) (response *ApplicationResponseData, err error) {
+	// TODO: implement this.
 
 	return
 }
--- integrations/identitymind/consumer/request.go
+++ integrations/identitymind/consumer/request.go
@@ -1,6 +1,7 @@
 package consumer
 
 import (
+	"encoding/json"
 	"strings"
 
 	"gitlab.com/lambospeed/kyc/common"
@@ -140,7 +141,7 @@ type KYCRequestData struct {
 }
 
 // setApplicantSSN sets ApplicantSSN field in KYCRequestData in the required format.
-func (k *KYCRequestData) setApplicantSSN(doc *common.DocumentMetadata) {
+func (r *KYCRequestData) setApplicantSSN(doc *common.DocumentMetadata) {
 	if doc.Type != common.IDCard {
 		return
 	}
@@ -149,56 +150,19 @@ func (k *KYCRequestData) setApplicantSSN(doc *common.DocumentMetadata) {
 	b.WriteString(":")
 	b.WriteString(doc.Number)
 
-	k.ApplicantSSN = b.String()
+	r.ApplicantSSN = b.String()
 }
 
-// ApplicationResponseData defines the model for Response Data for a Consumer or Merchant KYC.
-type ApplicationResponseData struct {
-	// The current reputation of the user involved in this transaction.
-	CurrentUserReputation EDNAPolicyResult `json:"user"`
-	// The previous reputation of the User, that is, the reputation of the user the last time that it was evaluated.
-	PreviousUserReputation EDNAPolicyResult `json:"upr"`
-	// FIXME: do we really need this?
-	// ednaScoreCard:	ExternalizedTransactionScorecard{...}
-	// The name of the fraud rule that fired.
-	FraudRuleName string `json:"frn"`
-	// Result of fraud evaluation.
-	FraudPolicyResult FraudPolicyResult `json:"frp"`
-	// The description of the fraud rule that fired.
-	FraudRuleDescription string `json:"frd"`
-	// Result of automated review evaluation.
-	ARPResult AutomatedReviewPolicyResult `json:"arpr"`
-	// The description, if any, of the automated review rule that fired.
-	ARPDescription string `json:"arpd"`
-	// The id, if any, of the automated review rule that fired.
-	ARPID string `json:"arpid"`
-	// FIXME: do we really need this?
-	// graphScore:	GraphRiskScore{...}
-	// The transaction id for this KYC. This id should be provided on subsequent updates to the KYC.
-	KYCTxID string `json:"mtid"`
-	// The current state of the KYC. A - Accepted; R - Under Review; D - Rejected.
-	State KYCState `json:"state"`
-	// FIXME: do we really need this?
-	// oowQuestions:	QuestionsWrapper{...}
-	ACVerification string `json:"acVerification"`
-	// FIXME: do we really need this?
-	DocVerification     *DocumentVerification `json:"docVerification"`
-	SMSVerification     string                `json:"smsVerification"`
-	OwnerApplicationIDs []string              `json:"ownerApplicationIds"`
-	ParentMerchant      string                `json:"parentMerchant"`
-	MerchantAPIName     string                `json:"merchantAPIName"`
-	// A description of the reason for the User’s reputation.
-	ReputationReasonDescription string `json:"erd"`
-	// Result of policy evaluation. Combines the result of fraud and automated review evaluations.
-	Result FraudPolicyResult `json:"res"`
-	// The set of result codes from the evaluation of the current transaction.
-	TxResultCodes string `json:"rcd"`
-	// The transaction id of the current transaction. If no “tid” data was provided in the request data then a unique id will be generated. No assumptions should be made about the format of the generated id and it will be a maximimum length of 64 alphanumeric characters.
-	TxID string `json:"tid"`
+// populateFields populate the fields of the object with input data.
+func (r *KYCRequestData) populateFields(customer *common.UserData) (err error) {
+	// TODO: implement this.
+
+	return
 }
 
-// DocumentVerification is the part of ApplicationResponseData.
-type DocumentVerification struct {
-	RedirectURL string `json:"redirectURL"`
-	RequestID   string `json:"requestId"`
+// createRequestBody creates request body from the object data.
+func (r *KYCRequestData) createRequestBody() (body []byte, err error) {
+	body, err = json.Marshal(r)
+
+	return
 }
