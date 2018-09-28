bb45b856707b7e1938d47258ab72f91088238d7c
Zoran Rasic <zokiupwork@gmail.com>
2018-09-28 03:38:55 +0300

    UserData: big changes in common package. Tests should fail so far.
    * removed document types enum - no need anymore
    * changed and unified KYC result model
    * changed and improved result's enums
    * added enum for banking card types
    * changed UserData model
    * removed abstract model for documents - no need anymore
    * added specialized models for documents

--- common/contract.go
+++ common/contract.go
@@ -2,5 +2,5 @@ package common
 
 // CustomerChecker defines the interface for a KYC verificator.
 type CustomerChecker interface {
-	CheckCustomer(customer *UserData) (KYCResult, *DetailedKYCResult, error)
+	CheckCustomer(customer *UserData) (KYCResult, error)
 }
