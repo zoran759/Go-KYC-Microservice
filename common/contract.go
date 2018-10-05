2fab017be19c82d098fafefccfc1fe87a7091d6b
Zoran Rasic <zokiupwork@gmail.com>
2018-10-05 03:29:34 +0300

    Polling exposure: the interface for status polling.
    * The controversial issue on Sum&Sub verification implementation.

--- common/contract.go
+++ common/contract.go
@@ -1,6 +1,14 @@
 package common
 
-// CustomerChecker defines the interface for a KYC verificator.
+// CustomerChecker represents a KYC verificator.
 type CustomerChecker interface {
 	CheckCustomer(customer *UserData) (KYCResult, error)
+}
+
+// StatusChecker represents a KYC verification status checker.
+// A CheckStatus checks the status of the KYC verification using
+// KYCProvider param for KYC provider's name for which the check should be performed
+// and second param as the submission id returned from a KYC provider API.
+type StatusChecker interface {
+	CheckStatus(KYCProvider, string) (KYCResult, error)
 }
