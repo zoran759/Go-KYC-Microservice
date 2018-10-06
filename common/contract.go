42d57d6124857cb2be5a38e972badcf9d37f5822
Zoran Rasic <zokiupwork@gmail.com>
2018-10-06 17:13:09 +0300

    Polling exposure: the implementation.

--- common/contract.go
+++ common/contract.go
@@ -7,8 +7,7 @@ type CustomerChecker interface {
 
 // StatusChecker represents a KYC verification status checker.
 // A CheckStatus checks the status of the KYC verification using
-// KYCProvider param for KYC provider's name for which the check should be performed
-// and second param as the submission id returned from a KYC provider API.
+// input param as the submission id returned from a KYC provider API.
 type StatusChecker interface {
-	CheckStatus(KYCProvider, string) (KYCResult, error)
+	CheckStatus(string) (KYCResult, error)
 }
