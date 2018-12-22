6108b64c9e27f0cd5938d00c1f2887c48767a9fa
Zoran Rasic <zokiupwork@gmail.com>
2018-12-23 01:16:06 +0300

    ThomsonReuters: models for the API.

--- integrations/thomsonreuters/model/provider.go
+++ integrations/thomsonreuters/model/provider.go
@@ -7,39 +7,29 @@ const (
 	HiddenSource  ProviderSourceStatus = "HIDDEN"
 )
 
+// List of ProviderType values.
+const (
+	WatchList       ProviderType = "WATCHLIST"
+	PassportCheck   ProviderType = "PASSPORT_CHECK"
+	ClientWatchList ProviderType = "CLIENT_WATCHLIST"
+)
+
 // List of SubscriptionCategory values.
 const (
 	Premium  SubscriptionCategory = "PREMIUM"
 	Standard SubscriptionCategory = "STANDARD"
 )
 
-// ProviderSourceStatus represents the provider source status enumeration.
-type ProviderSourceStatus string
+// ProviderDetails represents a list of available providers and their sources.
+type ProviderDetails []ProviderDetail
 
-// SubscriptionCategory represents the subscription category type enumeration.
-type SubscriptionCategory string
-
-// ProviderSourceTypeCategoryDetail represents a provider source type category detail.
-type ProviderSourceTypeCategoryDetail struct {
-	ID                  string               `json:"identifier"`
-	Name                string               `json:"name"`
-	Description         string               `json:"description"`
-	ProviderSourceTypes []ProviderSourceType `json:"providerSourceTypes"`
-}
-
-// ProviderSourceType represents a provider source type.
-type ProviderSourceType struct {
-	ID       string                           `json:"identifier"`
-	Name     string                           `json:"name"`
-	Category ProviderSourceTypeCategoryDetail `json:"category"`
-}
-
-// Provider represents a provider for a source.
-type Provider struct {
-	ID     string `json:"identifier"`
-	Code   string `json:"code"`
-	Name   string `json:"name"`
-	Master bool   `json:"master"`
+// ProviderDetail represents a provider.
+type ProviderDetail struct {
+	ID      string           `json:"identifier"`
+	Code    string           `json:"code"`
+	Name    string           `json:"name"`
+	Master  bool             `json:"master"`
+	Sources []ProviderSource `json:"sources"`
 }
 
 // ProviderSource represents a source that belongs to a provider.
@@ -52,18 +42,38 @@ type ProviderSource struct {
 	Type                 ProviderSourceType   `json:"type"`
 	ProviderSourceStatus ProviderSourceStatus `json:"providerSourceStatus"`
 	RegionOfAuthority    string               `json:"regionOfAuthority"`
-	CreationDate         string               `json:"creationDate"`
 	SubscriptionCategory SubscriptionCategory `json:"subscriptionCategory"`
+	CreationDate         string               `json:"creationDate"`
 }
 
-// ProviderDetail represents a provider.
-type ProviderDetail struct {
-	ID      string           `json:"identifier"`
-	Code    string           `json:"code"`
-	Name    string           `json:"name"`
-	Master  bool             `json:"master"`
-	Sources []ProviderSource `json:"sources"`
+// Provider represents a provider for a source.
+type Provider struct {
+	ID     string `json:"identifier"`
+	Code   string `json:"code"`
+	Name   string `json:"name"`
+	Master bool   `json:"master"`
 }
 
-// ProviderDetails represents a list of available providers and their sources.
-type ProviderDetails []ProviderDetail
+// ProviderSourceStatus represents the provider source status enumeration.
+type ProviderSourceStatus string
+
+// SubscriptionCategory represents the subscription category type enumeration.
+type SubscriptionCategory string
+
+// ProviderSourceType represents a provider source type.
+type ProviderSourceType struct {
+	ID       string                           `json:"identifier"`
+	Name     string                           `json:"name"`
+	Category ProviderSourceTypeCategoryDetail `json:"category"`
+}
+
+// ProviderSourceTypeCategoryDetail represents a provider source type category detail.
+type ProviderSourceTypeCategoryDetail struct {
+	ID                  string               `json:"identifier"`
+	Name                string               `json:"name"`
+	Description         string               `json:"description"`
+	ProviderSourceTypes []ProviderSourceType `json:"providerSourceTypes"`
+}
+
+// ProviderType represents the provider type enumeration.
+type ProviderType string
