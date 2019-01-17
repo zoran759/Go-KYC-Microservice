1e5db7943e23c53ee4abddbbc9d93a3df8ddd5cb
Zoran Rasic <zokiupwork@gmail.com>
2019-01-17 18:38:43 +0300

    Config: adds service configuration support. Adds cli flags support.

--- main/config/config.go
+++ main/config/config.go
@@ -1,15 +1,40 @@
 package config
 
-import (
-	"modulus/kyc/common"
+const (
+	// ServiceSection is the hardcoded value of the KYC service config section name.
+	ServiceSection = "Config"
+
+	// DefaultPort is default port of the KYC service.
+	DefaultPort = "8080"
 )
 
-// KYC holds the current config for KYC providers.
+// Cfg holds the current config for the KYC service.
 // Beware that it isn't concurrent writes safe.
-var KYC Config
+var Cfg Config
 
 // Options represents the configuration options for the KYC provider.
 type Options map[string]string
 
-// Config represents the configuration for KYC providers.
-type Config map[common.KYCProvider]Options
+// Config represents the configuration for the service.
+type Config map[string]Options
+
+// Option tries to retrieve requested option from the specified config section.
+// If the option doesn't exist the empty value is returned.
+func (c Config) Option(section, option string) (opt string) {
+	s, ok := c[section]
+	if !ok {
+		return
+	}
+
+	opt = s[option]
+
+	return
+}
+
+// ServicePort returns the KYC service port.
+func (c Config) ServicePort() (port string) {
+	if port = c.Option(ServiceSection, "Port"); len(port) == 0 {
+		port = DefaultPort
+	}
+	return
+}
