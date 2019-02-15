7a3f8b39c06ef75950b1b487fbf2997c6f645c80
Zoran Rasic <zokiupwork@gmail.com>
2019-02-15 03:16:24 +0300

    Coinfirm: modifies participant model to support corporate users.

--- integrations/coinfirm/model/participant.go
+++ integrations/coinfirm/model/participant.go
@@ -3,8 +3,11 @@ package model
 // DateFormat is the date representation form appropriate for the API.
 const DateFormat = "2006-01-02"
 
-// Individual is a value of participantType.
-const Individual participantType = "individual"
+// List of participantType type values.
+const (
+	Corporate  participantType = "corporate"
+	Individual participantType = "individual"
+)
 
 // NewParticipant represents a new participant without data.
 // A new participant creation has to be requested first then send details if succeed.
@@ -19,31 +22,33 @@ type NewParticipantResponse struct {
 	UUID string `json:"uuid"`
 }
 
-// ParticipantDetails represents an individual participant data.
+// ParticipantDetails represents a participant data.
 // They have to be send after successful creation of the new participant.
 type ParticipantDetails struct {
 	UserIP        string          `json:"user_ip,omitempty"`
-	Type          participantType `json:"type"`
+	Type          participantType `json:"type,omitempty"`
 	Pep           bool            `json:"pep,omitempty"`
-	FirstName     string          `json:"first_name"`
-	LastName      string          `json:"last_name"`
+	FirstName     string          `json:"first_name,omitempty"`
+	LastName      string          `json:"last_name,omitempty"`
 	MiddleName    string          `json:"middle_name,omitempty"`
-	Email         string          `json:"email"`
-	Nationality   string          `json:"nationality"`
-	IDNumber      string          `json:"id_number"`
-	Phone         string          `json:"phone"`
-	PhoneVerified bool            `json:"phone_verified"`
-	Country       string          `json:"country"`
-	Postcode      string          `json:"postcode"`
+	Email         string          `json:"email,omitempty"`
+	Website       string          `json:"website,omitempty"`
+	Nationality   string          `json:"nationality,omitempty"`
+	IDNumber      string          `json:"id_number,omitempty"`
+	Phone         string          `json:"phone,omitempty"`
+	PhoneVerified bool            `json:"phone_verified,omitempty"`
+	Country       string          `json:"country,omitempty"`
+	Postcode      string          `json:"postcode,omitempty"`
 	City          string          `json:"city,omitempty"`
 	Street        string          `json:"street,omitempty"`
-	BirthDate     string          `json:"bdate"`
+	BirthDate     string          `json:"bdate,omitempty"`
 	FileFundsText string          `json:"fileFundsText,omitempty"`
 	Sow           *Sow            `json:"sow,omitempty"`
-	Custom        []string        `json:"custom,omitempty"`
+	Beneficials   []Beneficial    `json:"beneficials,omitempty"`
+	Custom        []interface{}   `json:"custom,omitempty"`
 }
 
-// participantType represents participant types.
+// participantType represents participant type enumeration.
 type participantType string
 
 // Sow represents sow.
@@ -56,4 +61,16 @@ type Sow struct {
 	CryptoTrading      bool `json:"sow_crypto_trading"`
 	ICOContribution    bool `json:"sow_ico_contribution"`
 	Other              bool `json:"sow_other"`
+}
+
+// Beneficial represents a beneficial.
+type Beneficial struct {
+	Name              string `json:"beneficial_name,omitempty"`
+	SowRealEstateSale bool   `json:"sow_real_estate_sale,omitempty"`
+	Pep               bool   `json:"beneficial_pep,omitempty"`
+	NationAlpha3      string `json:"beneficial_nation,omitempty"`
+	Address           string `json:"beneficial_adr,omitempty"`
+	BirthDate         string `json:"beneficial_bdate,omitempty"`
+	Proc              int    `json:"beneficial_proc,omitempty"`
+	WealthText        string `json:"beneficial_wealth_text,omitempty"`
 }
