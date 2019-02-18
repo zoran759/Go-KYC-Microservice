f9d55389ac5628321a1f80d913a7714050f4fe49
Zoran Rasic <zokiupwork@gmail.com>
2019-02-18 20:43:09 +0300

    IDM: fixes tests.

--- integrations/identitymind/consumer/client_test.go
+++ integrations/identitymind/consumer/client_test.go
@@ -184,6 +184,19 @@ var underReviewResponse = `
                 "ruleId": 30060,
                 "testResults": [
                     {
+                        "test": "dv:20",
+                        "fired": false,
+                        "details": "Country of Document submitted does  match country of document requested.",
+                        "condition": {
+                            "left": "dv:20",
+                            "right": false,
+                            "operator": "eq",
+                            "type": "info"
+                        },
+                        "ts": 1550361265132,
+                        "stage": "1"
+                    },
+                    {
                         "test": "dv:1",
                         "fired": true,
                         "details": "[Fired] No remaining queries for this third party service. Please increase limit in the Admin tab of the UI. Select the Merchant Preferences section and 'Third Party Overview'",
@@ -252,6 +265,7 @@ var rejectedResponse = `
 {
     "ednaScoreCard": {
         "er": {
+            "profile": "DEFAULT",
             "reportedRule": {
                 "description": "Fake fraud attempt simulation.",
                 "details": "Fake transaction with the suspicious entity. No other rules triggered.",
@@ -306,6 +320,139 @@ var unknownStateResponse = `
     "tid": "26860023",
     "upr": "UNKNOWN",
     "user": "Unknown"
+}`
+
+var needStatusPollingResponse = `
+{
+    "user": "UNKNOWN",
+    "upr": "UNKNOWN",
+    "ednaScoreCard": {
+        "sc": [],
+        "etr": [
+            {
+                "test": "ed:23",
+                "fired": false,
+                "details": "Associated Devices: 0",
+                "ts": 1550365541000,
+                "stage": "1"
+            },
+            {
+                "test": "ed:37",
+                "fired": false,
+                "details": "ed:37(false) = true",
+                "ts": 1550365541000,
+                "stage": "1"
+            },
+            {
+                "test": "ed:59",
+                "fired": false,
+                "details": "The MAN count is 1",
+                "ts": 1550365541000,
+                "stage": "1"
+            },
+            {
+                "test": "ed:36",
+                "fired": false,
+                "details": "ed:36(false) = true",
+                "ts": 1550365541000,
+                "stage": "1"
+            },
+            {
+                "test": "ed:25",
+                "fired": false,
+                "details": "Associated Billing Addresses: 1",
+                "ts": 1550365541000,
+                "stage": "1"
+            },
+            {
+                "test": "ss:0",
+                "details": "false",
+                "ts": 1550365541000,
+                "stage": "1"
+            },
+            {
+                "test": "mm:0",
+                "details": "false",
+                "ts": 1550365541000,
+                "stage": "1"
+            },
+            {
+                "test": "au:1",
+                "details": "false",
+                "ts": 1550365541000,
+                "stage": "1"
+            },
+            {
+                "test": "dv:1",
+                "waitingForData": true,
+                "ts": 1550365541000,
+                "stage": "1"
+            },
+            {
+                "test": "ed:32",
+                "fired": false,
+                "details": "ed:32(false) = true",
+                "ts": 1550365541000,
+                "stage": "1"
+            },
+            {
+                "test": "dv:0",
+                "waitingForData": true,
+                "ts": 1550365541000,
+                "stage": "1"
+            },
+            {
+                "test": "ra:0",
+                "details": "false",
+                "ts": 1550365541000,
+                "stage": "1"
+            },
+            {
+                "test": "ed:2",
+                "fired": false,
+                "details": "No transaction parameter on watchlist",
+                "ts": 1550365541000,
+                "stage": "1"
+            },
+            {
+                "test": "ed:1",
+                "fired": false,
+                "details": "ed:1(false) = true",
+                "ts": 1550365541000,
+                "stage": "1"
+            },
+            {
+                "test": "ed:19",
+                "fired": false,
+                "details": "User Account reputation is Bad or Blacklisted",
+                "ts": 1550365541000,
+                "stage": "1"
+            }
+        ],
+        "er": {
+            "profile": "DEFAULT",
+            "reportedRule": {
+                "description": "Fallthrough for transaction with an unknown entity. No other rules triggered.",
+                "details": "Fallthrough for transaction with an unknown entity. No other rules triggered.",
+                "ruleId": 1002,
+                "testResults": [],
+                "resultCode": "ACCEPT",
+                "name": "Unknown Fallthrough"
+            }
+        }
+    },
+    "frn": "Unknown Fallthrough",
+    "frp": "ACCEPT",
+    "frd": "Fallthrough for transaction with an unknown entity. No other rules triggered.",
+    "mtid": "30ca28c4d39b4211840d4b4d2670afc1",
+    "state": "R",
+    "docVerification": {
+        "requestId": "1543612172237-178cc1c1-30ca28c4d39b4211840d4b4d2670afc1"
+    },
+    "tid": "30ca28c4d39b4211840d4b4d2670afc1",
+    "res": "ACCEPT",
+    "erd": "Unknown User",
+    "rcd": "131,50005,150,104,202,1002"
 }`
 
 var _ = Describe("Client", func() {
@@ -403,13 +550,7 @@ var _ = Describe("Client", func() {
 			result, err := client.CheckCustomer(&common.UserData{})
 
 			Expect(result.Status).To(Equal(common.Approved))
-			Expect(result.Details).NotTo(BeNil())
-			Expect(result.Details.Finality).To(Equal(common.Unknown))
-			Expect(result.Details.Reasons).To(HaveLen(4))
-			Expect(result.Details.Reasons[0]).To(Equal("Customer reputation: UNKNOWN"))
-			Expect(result.Details.Reasons[1]).To(Equal("Fraud policy evaluation result: ACCEPT"))
-			Expect(result.Details.Reasons[2]).To(Equal("Customer reputation reason: Unknown User"))
-			Expect(result.Details.Reasons[3]).To(Equal("Combined fraud and automated review evaluations result: ACCEPT"))
+			Expect(result.Details).To(BeNil())
 			Expect(err).ToNot(HaveOccurred())
 		})
 
@@ -423,11 +564,14 @@ var _ = Describe("Client", func() {
 			Expect(result.Status).To(Equal(common.Denied))
 			Expect(result.Details).NotTo(BeNil())
 			Expect(result.Details.Finality).To(Equal(common.Unknown))
-			Expect(result.Details.Reasons).To(HaveLen(4))
+			Expect(result.Details.Reasons).To(HaveLen(7))
 			Expect(result.Details.Reasons[0]).To(Equal("Customer reputation: BAD"))
-			Expect(result.Details.Reasons[1]).To(Equal("Fraud policy evaluation result: DENY"))
-			Expect(result.Details.Reasons[2]).To(Equal("Customer reputation reason: Fraudster"))
+			Expect(result.Details.Reasons[1]).To(Equal("Reputation reason: Fraudster"))
+			Expect(result.Details.Reasons[2]).To(Equal("Fraud policy evaluation result: DENY"))
 			Expect(result.Details.Reasons[3]).To(Equal("Combined fraud and automated review evaluations result: DENY"))
+			Expect(result.Details.Reasons[4]).To(Equal("Triggered status: DENY"))
+			Expect(result.Details.Reasons[5]).To(Equal("Profile: DEFAULT"))
+			Expect(result.Details.Reasons[6]).To(Equal("Rule: id 1002 | Fake fraud attempt simulation."))
 			Expect(err).ToNot(HaveOccurred())
 		})
 
@@ -439,13 +583,7 @@ var _ = Describe("Client", func() {
 			result, err := client.CheckCustomer(&common.UserData{})
 
 			Expect(result.Status).To(Equal(common.Error))
-			Expect(result.Details).NotTo(BeNil())
-			Expect(result.Details.Finality).To(Equal(common.Unknown))
-			Expect(result.Details.Reasons).To(HaveLen(4))
-			Expect(result.Details.Reasons[0]).To(Equal("Customer reputation: Unknown"))
-			Expect(result.Details.Reasons[1]).To(Equal("Fraud policy evaluation result: DENY"))
-			Expect(result.Details.Reasons[2]).To(Equal("Customer reputation reason: Fraudster"))
-			Expect(result.Details.Reasons[3]).To(Equal("Combined fraud and automated review evaluations result: DENY"))
+			Expect(result.Details).To(BeNil())
 			Expect(err).To(HaveOccurred())
 			Expect(err.Error()).To(Equal("unknown state of the verification from the API: U"))
 		})
@@ -460,13 +598,30 @@ var _ = Describe("Client", func() {
 			Expect(result.Status).To(Equal(common.Denied))
 			Expect(result.Details).NotTo(BeNil())
 			Expect(result.Details.Finality).To(Equal(common.Unknown))
-			Expect(result.Details.Reasons).To(HaveLen(5))
-			Expect(result.Details.Reasons[0]).To(Equal("Some of checks triggered 'MANUAL REVIEW' status"))
-			Expect(result.Details.Reasons[1]).To(Equal("Profile: DEFAULT"))
-			Expect(result.Details.Reasons[2]).To(Equal("Rule: id 30060 | ID document validation"))
-			Expect(result.Details.Reasons[3]).To(Equal("Test: 'dv:1' | [Fired] No remaining queries for this third party service. Please increase limit in the Admin tab of the UI. Select the Merchant Preferences section and 'Third Party Overview'"))
-			Expect(result.Details.Reasons[4]).To(Equal("Test: 'dv:0' | [Fired] No remaining queries for this third party service. Please increase limit in the Admin tab of the UI. Select the Merchant Preferences section and 'Third Party Overview'"))
+			Expect(result.Details.Reasons).To(HaveLen(6))
+			Expect(result.Details.Reasons[0]).To(Equal("MANUAL REVIEW REQUIRED"))
+			Expect(result.Details.Reasons[1]).To(Equal("Triggered status: MANUAL_REVIEW"))
+			Expect(result.Details.Reasons[2]).To(Equal("Profile: DEFAULT"))
+			Expect(result.Details.Reasons[3]).To(Equal("Rule: id 30060 | ID document validation"))
+			Expect(result.Details.Reasons[4]).To(Equal("Test: 'dv:1' | [Fired] No remaining queries for this third party service. Please increase limit in the Admin tab of the UI. Select the Merchant Preferences section and 'Third Party Overview'"))
+			Expect(result.Details.Reasons[5]).To(Equal("Test: 'dv:0' | [Fired] No remaining queries for this third party service. Please increase limit in the Admin tab of the UI. Select the Merchant Preferences section and 'Third Party Overview'"))
 			Expect(result.StatusCheck).To(BeNil())
+			Expect(err).ToNot(HaveOccurred())
+		})
+
+		It("should success with status polling data returned", func() {
+			Expect(client).ToNot(BeNil())
+
+			httpmock.RegisterResponder(http.MethodPost, client.host+consumerEndpoint, httpmock.NewStringResponder(http.StatusOK, needStatusPollingResponse))
+
+			result, err := client.CheckCustomer(&common.UserData{})
+
+			Expect(result.Status).To(Equal(common.Unclear))
+			Expect(result.Details).To(BeNil())
+			Expect(result.StatusCheck).NotTo(BeNil())
+			Expect(result.StatusCheck.Provider).To(Equal(common.IdentityMind))
+			Expect(result.StatusCheck.ReferenceID).To(Equal("30ca28c4d39b4211840d4b4d2670afc1"))
+			Expect(result.StatusCheck.LastCheck).NotTo(BeZero())
 			Expect(err).ToNot(HaveOccurred())
 		})
 	})
